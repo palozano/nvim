e06bfe33a41bbb6b06cde1626193c6323eebbb77
Pablo Lozano <pablo.lozano@kraken.com>
2025-12-12 14:02:03 +0100

    keymaps for terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -7,13 +7,18 @@ return {
       keys = {
         {
           "<leader>tt",
+          "<cmd>ToggleTerm<cr>",
+          desc = "Toggle terminal",
+        },
+        {
+          "<leader>tb",
           "<cmd>ToggleTerm direction=horizontal<cr>",
-          desc = "Terminal (bottom)",
+          desc = "Toggle terminal (bottom)",
         },
         {
           "<leader>tf",
           "<cmd>ToggleTerm direction=float<cr>",
-          desc = "Terminal (float)",
+          desc = "Toggle terminal (float)",
         },
       }
     }
