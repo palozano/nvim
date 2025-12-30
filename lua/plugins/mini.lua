f27520407c2c35a4367d9e9c45ec206310a49dd6
Pablo Lozano <pablo.lozano@kraken.com>
2025-12-30 15:48:14 +0100

    remove gitsigns, use mini.diff

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,3 +11,7 @@ require("mini.surround").setup()
 -- improve UI
 require("mini.cursorword").setup()
 require("mini.icons").setup()
+-- git improvements
+require("mini.diff").setup()
+
+vim.keymap.set("n", "<leader>to", function() require("mini.diff").toggle_overlay(0) end, { desc = "Toggle diff overlay" })
