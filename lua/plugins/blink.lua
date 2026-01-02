b6f1820047b3ac4b3aa7e74e2919083858a76fe2
Pablo Lozano <nivolabs@pm.me>
2026-01-02 16:40:51 +0100

    emojis in nvim

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,7 +3,9 @@ vim.pack.add({
     src = "https://github.com/saghen/blink.cmp",
     version = vim.version.range("^1")
   },
+  -- dependencies for blink
   { src = "https://github.com/rafamadriz/friendly-snippets" },
+  { src = "https://github.com/moyiz/blink-emoji.nvim" },
 })
 
 local blink = require("blink.cmp")
@@ -23,7 +25,23 @@ blink.setup({
     enabled = true,
   },
   sources = {
-    default = { "lsp", "path", "snippets", "buffer" },
+    default = { "lsp", "path", "snippets", "buffer", "emoji" },
+    providers = {
+      emoji = {
+        module = "blink-emoji",
+        name = "Emoji",
+        score_offset = 15, -- tune by preference
+        opts = { insert = true },
+        should_show_items = function()
+          return vim.tbl_contains(
+          -- Enable emoji completion only for git commits and markdown.
+          -- By default, enabled for all file-types.
+            { "gitcommit", "markdown" },
+            vim.o.filetype
+          )
+        end
+      }
+    }
   },
   completion = {
     documentation = {
---
+++ lua/plugins/csvview.lua
@@ -0,0 +1,5 @@
+vim.pack.add({
+  { src = "https://github.com/hat0uma/csvview.nvim" },
+})
+
+require("csvview.nvim").setup()
